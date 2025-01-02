0f572501b5b5d16f2220f8917492903e716f2ad2
devxprite <devxprite@gmail.com>
2025-01-02 10:48:27 +0530

    Update

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,6 +3,8 @@ import { Button } from './ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
 import { Input } from './ui/input';
 import { RainbowButton } from './ui/rainbow-button';
+import AnimatedGridPattern from './ui/animated-grid-pattern';
+import ShineBorder from './ui/shine-border';
 
 type Props = {
     isLoading: boolean;
@@ -18,11 +20,18 @@ const Welcome = ({ onSubmit, isLoading }: Props) => {
     };
 
     return (
-        <div className="min-h-screen">
+        <div className="min-h-screen relative">
+            <AnimatedGridPattern
+                numSquares={50}
+                maxOpacity={0.1}
+                duration={3}
+                repeatDelay={1}
+                className={'[mask-image:linear-gradient(to_top,#fffb,transparent)]'}
+            />
             <div className="max-w-screen-xl mx-auto px-4 py-8 min-h-screen flex items-center justify-center">
                 <div>
                     <h1 className="text-4xl leading-[1.1] font-semibold text-center">
-                        Collection of modern, background snippets
+                        <span className="text-gradient">Collection of modern</span>, background snippets
                     </h1>
                     <p className="text-muted-foreground mt-6 max-w-screen-sm mx-auto text-center text-xl">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident hic soluta laudantium
@@ -34,8 +43,13 @@ const Welcome = ({ onSubmit, isLoading }: Props) => {
                             <Loader2 className="size-14 animate-spin " />
                         </div>
                     ) : (
-                        <form className="mt-20" onSubmit={handleSubmit}>
-                            <Card className="max-w-lg m-auto">
+                        <ShineBorder
+                            className="mt-12 p-8 md:p-12 bg-red-400 bg-muted/0 relative w-full max-w-screen-lg"
+                            color={['#fb923c', '#ec4899', '#9333ea']}
+                            borderWidth={2}
+                            borderRadius={24}
+                        >
+                            <form onSubmit={handleSubmit} className="w-full">
                                 <CardHeader>
                                     <CardTitle>Join the Chat</CardTitle>
                                     <CardDescription>Enter your name to join the chat</CardDescription>
@@ -48,8 +62,8 @@ const Welcome = ({ onSubmit, isLoading }: Props) => {
                                         Join the Chat <ArrowRight />{' '}
                                     </Button>
                                 </CardFooter>
-                            </Card>
-                        </form>
+                            </form>
+                        </ShineBorder>
                     )}
                 </div>
             </div>
