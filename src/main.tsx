9b4c5d602d84a4aff7e51da96593125de0ad7b0f
devxprite <devxprite@gmail.com>
2025-01-12 11:50:54 +0530

    Add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { lazy, StrictMode, Suspense } from 'react';
 import { SettingsProvider } from './context/settings-context.tsx';
 import { AnimatePresence } from 'motion/react';
 import { DefaultToastOptions, Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 
 const AuthProvider = lazy(() => import('./context/auth-context').then(module => ({ default: module.AuthProvider })));
 
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
                 <SettingsProvider>
                     <AnimatePresence>
                         <App />
+                        <Analytics />
                         <Toaster toastOptions={toastOptions} />
                     </AnimatePresence>
                 </SettingsProvider>
