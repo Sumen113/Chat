d666977ae42c5aa348ea584b1379a1ba85129739
devxprite <devxprite@gmail.com>
2025-01-19 17:35:05 +0530

    chore: Add speed-insights package

--- src/main.tsx
+++ src/main.tsx
@@ -5,28 +5,27 @@ import { createRoot } from 'react-dom/client';
 import { lazy, StrictMode, Suspense } from 'react';
 import { SettingsProvider } from './context/settings-context.tsx';
 import { AnimatePresence } from 'motion/react';
-import { DefaultToastOptions, Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import { Analytics } from '@vercel/analytics/react';
-import ReactGA from "react-ga4";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import ReactGA from 'react-ga4';
 
 const AuthProvider = lazy(() => import('./context/auth-context').then(module => ({ default: module.AuthProvider })));
 
-const toastOptions: DefaultToastOptions = {
-    success: {},
-};
-
-ReactGA.initialize("G-KEZHDV7Y1X");
-
+ReactGA.initialize('G-KEZHDV7Y1X');
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <Analytics />
+        <SpeedInsights />
+
         <Suspense key={'layout'} fallback={<Loader />}>
             <AuthProvider>
                 <SettingsProvider>
                     <AnimatePresence>
                         <App />
-                        <Analytics />
-                        <Toaster toastOptions={toastOptions} />
+
+                        <Toaster />
                     </AnimatePresence>
                 </SettingsProvider>
             </AuthProvider>
